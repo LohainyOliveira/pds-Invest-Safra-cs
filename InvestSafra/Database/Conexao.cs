86ff3102c2159d2824c1c57e311c65b2aa068bdd
LohainyOliveira <oliveiradossantoslohainy@gmail.com>
2022-08-23 14:04:01 -0400

    arquivos iniciais

---
+++ InvestSafra/Database/Conexao.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace InvestSafra.Database
+{
+    internal class Conexao
+    {
+        private static string host = "localhost";
+
+        private static string port = "3360";
+
+        private static string user = "root";
+
+        private static string password = "root";
+
+        private static string dbname = "";
+
+        private static MySqlConnection connection;
+
+        private static MySqlCommand command;
+
+        public Conexao()
+        {
+            try
+            {
+                connection = new MySqlConnection($"server={host};database={dbname};port={port};user={user};password={password}");
+                connection.Open();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public MySqlCommand Query()
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                return command;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ InvestSafra/TelaPrincipalWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using InvestSafra.Views;
+
+
+namespace InvestSafra
+{
+	/// <summary>
+	/// Lógica interna para TelaPrincipalWindow.xaml
+	/// </summary>
+	public partial class TelaPrincipalWindow : Window
+	{
+		public TelaPrincipalWindow()
+		{
+			InitializeComponent();
+		}
+
+
+
+		private void btCadastra_Click(object sender, RoutedEventArgs e)
+		{
+			CadastroCliente form = new CadastroCliente();
+			form.ShowDialog();
+		}
+
+		private void btEntrar_Click(object sender, RoutedEventArgs e)
+		{
+
+			Entrar form = new Entrar();
+			form.ShowDialog();
+		}
+	}
+}
