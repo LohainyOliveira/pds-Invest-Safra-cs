26939fb12084d6a82928983297b0532ce0cdb728
LohainyOliveira <oliveiradossantoslohainy@gmail.com>
2022-08-25 01:22:59 -0300

    Atualização Models

---
+++ InvestSafra/Models/Empresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestSafra.Models
+{
+	public class Empresa
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string RazaoSocial { get; set; }
+		public string CNPJ { get; set; }
+		public string Responsavel { get; set; }
+		public string Telefone { get; set; }
+		public string Email { get; set; }
+		public string Estado { get; set; }
+		public string Cidade { get; set; }
+		public string Bairro { get; set; }
+		public string Rua { get; set; }
+		public string CEP { get; set; }
+		public string Complemento { get; set; }
+	}
+}
---
+++ InvestSafra/Models/Fazenda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestSafra.Models
+{
+	internal class Fazenda
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string NomeFantasia { get; set; }
+		public string CNPJ { get; set; }
+		public string Localizacao { get; set; }
+		public string Complemento { get; set; }
+	}
+}
