b4ae02aea875c50adfea5a887fee74d6f507bfe6
LohainyOliveira <oliveiradossantoslohainy@gmail.com>
2022-09-13 16:25:48 -0400

    update classe Clima

--- InvestSafra/Models/ClimaDAO.cs
+++ InvestSafra/Models/ClimaDAO.cs
@@ -3,10 +3,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using InvestSafra.Database;
+using InvestSafra.Helpers;
 
 namespace InvestSafra.Models
 {
 	internal class ClimaDAO
 	{
-	}
+        private static Conexao _conn = new Conexao();
+        public void Insert(Clima clima)
+        {
+
+
+            try
+            {
+                var comando = _conn.Query();
+
+                comando.CommandText = ("insert into Caixa value (null, @temperatura, @local, @clima)");
+
+
+
+                comando.Parameters.AddWithValue("@temperatura", clima.Temperatura);
+                comando.Parameters.AddWithValue("@local", clima.Local);
+                comando.Parameters.AddWithValue("@clima", clima.Climatizacao);
+
+                var resultado = comando.ExecuteNonQuery();
+
+
+
+                if (resultado == 0)
+                {
+                    throw new Exception("Ocorreram erros ao salvar as informações!!!");
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public List<Clima> List()
+        {
+            try
+            {
+
+                var lista = new List<Clima>();
+
+                var query = _conn.Query();
+                query.CommandText = "SELECT * FROM Clima";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+               
+                    var clima = new Clima();
+                    clima.Id = reader.GetInt32("id_clim");
+                    clima.Temperatura = DAOHelper.GetString(reader,"temperatura_clim");
+                    clima.Local = DAOHelper.GetString(reader, "local_clim");
+                    clima.Climatizacao = DAOHelper.GetString(reader, "clima_clim");
+                   
+
+                    lista.Add(clima);
+                }
+                reader.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Clima clima)
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+
+                comando.CommandText = "UPDATE Clima set  temperatura_clim = @temperatura, local_clim = @local, clima_clim = @clima WHERE id_clim = @id";
+
+
+
+                comando.Parameters.AddWithValue("@temperatura", clima.Temperatura);
+                comando.Parameters.AddWithValue("@local", clima.Local);
+                comando.Parameters.AddWithValue("@clima", clima.Climatizacao);
+
+              
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Delete(Clima clima)
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                comando.CommandText = "Delete from Clima where id_clim = @id";
+
+                comando.Parameters.AddWithValue("@id", clima.Id);
+
+                var resultado = comando.ExecuteNonQuery();
+
+                if (resultado == 0)
+                {
+                    throw new Exception("Ocorreu erros ao tentar deletar!");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
 }
