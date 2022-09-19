310dfe41ff5f24f7dbfb95fb76bc2f4e49188800
LohainyOliveira <oliveiradossantoslohainy@gmail.com>
2022-09-19 15:23:13 -0400

    Update

--- InvestSafra/Models/Fornecedor.cs
+++ InvestSafra/Models/Fornecedor.cs
@@ -8,6 +8,22 @@ namespace InvestSafra.Models
 {
 	internal class Fornecedor
 	{
+		/*
+create table Fornecedor (
+id_for int primary key auto_increment not null,
+nome_for varchar (100) not null,
+cnpj_for varchar (100) not null,
+razao_social_for varchar (100) not null,
+bairro_for varchar (100) not null,
+rua_for varchar (100) not null,
+cidade_for varchar (100) not null,
+estado_for varchar (100) not null,
+cep_for varchar (100) not null,
+complemento_for varchar (100) not null,
+telefone_pessoal_for varchar (100) not null,
+telefone_firma_for varchar (100) not null,
+email_for varchar (100) not null
+);*/
 		public int Id { get; set; }
 		public string CNPJ { get; set; }
 		public string Razao_Social { get; set; }
