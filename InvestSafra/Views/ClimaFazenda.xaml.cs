9015365ddec0be19fbb64c17117bca3a361385ec
KarlaReginaa <maisaekarla@gmail.com>
2022-10-03 09:35:30 -0400

    Ajustes Telas

--- InvestSafra/Views/ClimaFazenda.xaml.cs
+++ InvestSafra/Views/ClimaFazenda.xaml.cs
@@ -38,5 +38,38 @@ namespace InvestSafra.Views
             MessageBox.Show($"Campos Limpos com Sucesso", "Limpeza Concluida",
                 MessageBoxButton.OK, MessageBoxImage.Information);
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+        }
+
+        private bool IsMaxinized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaxinized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaxinized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaxinized = true;
+
+                }
+            }
+        }
     }
+
+    
 }
