e856243d4d6e400c7acf25e1285dbf587b21291b
Renata Castellan <renatacastellan200@gmail.com>
2022-10-03 09:44:13 -0400

    update botão salvar fornecdor

--- InvestSafra/Views/CadastrarFornecedor.xaml.cs
+++ InvestSafra/Views/CadastrarFornecedor.xaml.cs
@@ -20,7 +20,7 @@ namespace InvestSafra.Views
     /// </summary>
     public partial class CadastrarFornecedor : Window
     {
-        private CadastrarFornecedor _fornecedor = new CadastrarFornecedor();
+        private Fornecedor _fornecedor = new Fornecedor();
         public CadastrarFornecedor()
         {
             InitializeComponent();
@@ -33,10 +33,14 @@ namespace InvestSafra.Views
             txtComplemento.Clear();
             txtEmail.Clear();
             txtNomeFantasia.Clear();
-            txtSalario.Clear();
+            txtRazaoSocial.Clear();
             txtTelefone.Clear();
             txtTelefoneF.Clear();
             cbEstado.SelectedItem = null;
+            txtCep.Clear();
+            txtCidade.Clear();
+            txtEmail.Clear();
+            txtRua.Clear();
 
             ExibirMensagemLimpar();
         }
@@ -47,9 +51,52 @@ namespace InvestSafra.Views
                 MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
+        private void ExibirMensagemSalvar()
+        {
+            MessageBox.Show($"Campos Salvos com Sucesso!", "Registros Salvos",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
         private void btSalvar_Click(object sender, RoutedEventArgs e)
         {
-            
+            _fornecedor.Telefone_Pessoal = txtTelefone.Text;
+            _fornecedor.CEP = txtCep.Text;
+            _fornecedor.Bairro = txtBairro.Text;
+            _fornecedor.Cidade = txtCidade.Text;
+            _fornecedor.CNPJ = txtCNPJ.Text;
+            _fornecedor.Complemento = txtComplemento.Text;
+            _fornecedor.Email = txtComplemento.Text;
+            _fornecedor.Nome_Fantasia = txtNomeFantasia.Text;
+            _fornecedor.Razao_Social = txtRazaoSocial.Text;
+            _fornecedor.Rua = txtRua.Text;
+            _fornecedor.Telefone_Empresa = txtTelefoneF.Text;
+            _fornecedor.Estado = cbEstado.Text;
+
+            try
+            {
+                var dao = new FornecedorDAO();
+                dao.Insert(_fornecedor);
+
+                ExibirMensagemSalvar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            txtBairro.Clear();
+            txtCNPJ.Clear();
+            txtComplemento.Clear();
+            txtEmail.Clear();
+            txtNomeFantasia.Clear();
+            txtRazaoSocial.Clear();
+            txtTelefone.Clear();
+            txtTelefoneF.Clear();
+            cbEstado.SelectedItem = null;
+            txtCep.Clear();
+            txtCidade.Clear();
+            txtEmail.Clear();
+            txtRua.Clear();
         }
     }
 }
