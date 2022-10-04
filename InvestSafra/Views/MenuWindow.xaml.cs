dee8dcdb251a27805820643b2f11c9f3f6d43476
ANNACLIMAABREU <annalimaabreuclara@gmail.com>
2022-10-04 08:47:35 -0400

    Add icones e atualizar menu

--- InvestSafra/Views/MenuWindow.xaml.cs
+++ InvestSafra/Views/MenuWindow.xaml.cs
@@ -267,7 +267,12 @@ namespace InvestSafra.Views
 			CadastrarVenda form = new CadastrarVenda();
 			form.ShowDialog();
 		}
-		private void btSair_Click(object sender, RoutedEventArgs e)
+        private void SideMenuItemRecebimento_Selected(object sender, RoutedEventArgs e)
+        {
+            CadastrarRecebimentoWindow form = new CadastrarRecebimentoWindow();
+            form.ShowDialog();
+        }
+        private void btSair_Click(object sender, RoutedEventArgs e)
 		{
 			DialogResult = false;
 		}
