bf5840e40325eb2255a6f6764ff140ad5d5f35b9
KarlaReginaa <maisaekarla@gmail.com>
2022-11-16 15:06:08 -0400

    telas

--- InvestSafra/Models/Compra.cs
+++ InvestSafra/Models/Compra.cs
@@ -14,7 +14,7 @@ namespace InvestSafra.Models
 		public int Quantidade { get; set; }
 		public string Descricao { get; set; }
 		public DateTime? Data { get; set; }
-		public string Fornecedor { get; set; }
+		
 		
 	}
 }
